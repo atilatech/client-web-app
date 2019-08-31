21bf5aeeb321d8d82d2e3c153997add55a3af4b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-31 19:07:22 +0000

    Deploy atilatech/client-web-app to github.com/atilatech/client-web-app.git:gh-pages

---
+++ precache-manifest.492863822c66df59c12bc3e2475ad43c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58df125405f9c23be69a5610a34e7ee5",
+    "url": "/client-web-app/index.html"
+  },
+  {
+    "revision": "387c59483ad048710d27",
+    "url": "/client-web-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8e683e315cfd13aa82e0",
+    "url": "/client-web-app/static/css/main.9828ab43.chunk.css"
+  },
+  {
+    "revision": "387c59483ad048710d27",
+    "url": "/client-web-app/static/js/2.0d392f6f.chunk.js"
+  },
+  {
+    "revision": "8e683e315cfd13aa82e0",
+    "url": "/client-web-app/static/js/main.52c349f5.chunk.js"
+  },
+  {
+    "revision": "3a657be26326cca175f6",
+    "url": "/client-web-app/static/js/runtime~main.a873119c.js"
+  },
+  {
+    "revision": "04e1d2686c5610a30e50538f521013c8",
+    "url": "/client-web-app/static/media/atila-logo-circle-transparent.04e1d268.png"
+  }
+]);
