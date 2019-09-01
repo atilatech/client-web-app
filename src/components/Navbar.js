5e76d57fdd6a561c813c54bed85ba1fe0de9e91e
ademidun <tomiademidun@gmail.com>
2019-09-01 13:48:22 -0400

    updated navbar and commented out node_modules cache in .travis.yml

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import * as NavbarBootstrap from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
-import * as NavbarBootstrap from 'react-bootstrap/Navbar';
 
 import {Link} from "react-router-dom";
 
