318d85513a2071c8f6dd62c3312b9132a4950e88
ademidun <tomiademidun@gmail.com>
2022-01-11 22:47:19 -0500

    add quickstart instructions and fix navbar import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as NavbarBootstrap from 'react-bootstrap/Navbar';
+import NavbarBootstrap from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
